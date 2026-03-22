3eb15b4f1febca519ed5a38bf563a392b555258b
Yuriy Shulga <jsshu1ga@gmail.com>
2026-03-22 09:39:33 +0300

    Feat: add flat json file comparison

--- src/index.js
+++ src/index.js
@@ -1,5 +1,39 @@
-const gendiff = (file1, file2) => {
-  return `${file1}\n${file2}`
+import fs from 'fs'
+import path from 'path'
+import _ from 'lodash'
+const readFile = (filepath) => {
+  return fs.readFileSync(path.resolve(process.cwd(), filepath), 'utf-8')
+}
+
+const getDiff = (file1, file2) => {
+  const obj1 = JSON.parse(file1)
+  const obj2 = JSON.parse(file2)
+  const uniqKeys = _.sortBy(Object.keys({ ...obj1, ...obj2 }))
+  const diffs = uniqKeys.map((key) => {
+    if (!Object.hasOwn(obj1, key)) {
+      return `  + ${key}: ${obj2[key]}`
+    }
+
+    if (!Object.hasOwn(obj2, key)) {
+      return `  - ${key}: ${obj1[key]}`
+    }
+
+    if (obj1[key] === obj2[key]) {
+      return `    ${key}: ${obj1[key]}`
+    }
+
+    return `  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}`
+  })
+
+  return diffs
+}
+
+const gendiff = (filepath1, filepath2) => {
+  const file1 = readFile(filepath1)
+  const file2 = readFile(filepath2)
+
+  const diff = getDiff(file1, file2)
+  return `{\n${diff.join('\n')}\n}`
 }
 
 export default gendiff
